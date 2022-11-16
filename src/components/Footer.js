c64a31de018d6c61585912b74c633069d16e93a6
Aipoh Ruth <82672890+aipohruth@users.noreply.github.com>
2022-11-16 21:13:45 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ import twit from  '../Assets/twitter.png'
 const Footer = () => {
   return (
     <section id="footer">
-        <div className="container"> 
-    <div className="footer-row row bg-black">
+        
+    <div className="footer-row mr-2 row bg-black">
       <div className="col-12 col-lg-3 d-flex flex-column">
         <div className="p-3 p-lg-5">
           <img src={flogo} alt="metabnb" />
@@ -50,52 +50,7 @@ const Footer = () => {
       </div>
     </div>
 
-        {/*<div className="footer-row">
-            <div className="footer-col-1">
-                <img src={flogo} alt='Metabnb' className='footer-img'  width='150'/>
-                <div className="social-icons">
-                    <img src={fb} alt='facebook' width='25' />
-                    <img src={ig} alt='facebook' width='25' />
-                    <img src={twit} alt='facebook' width='25' />
-                </div>
-                
-                </div>
-                <div className="footer-col">
-                <h4>Community</h4>
-                <ul>
-                    <li>NFT</li>
-                    <li>Tokens</li>
-                    <li>Lanlords</li>
-                    <li>Discord</li>
-                </ul>
-                </div>
-    
-                <div className="footer-col">
-                <h4>Places</h4>
-                <ul>
-                    <li>castle</li>
-                    <li>farms</li>
-                    <li>beach</li>
-                    <li>learn more</li>
-                </ul>
-                </div>
-
-                <div className="footer-col">
-                <h4>About us</h4>
-                <ul>
-                    <li>Road Map</li>
-                    <li>Creators</li>
-                    <li>Carrer</li>
-                    <li>Contact us</li>
-                </ul>
-                </div>
-            
-
-
-                </div>
-  <p className='copy'>&copy; 2022 Metabnb</p>*/}
-
-                </div>
+        
                 </section>
   )
 }
